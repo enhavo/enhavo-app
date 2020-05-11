d4e9aa71c0c11622347eccc274bacffacdd02e73
Gerhard Seidel <gseidel.message@googlemail.com>
2020-05-11 11:03:38 +0800

    remove files, which will be installed over flex

--- assets/enhavo/dashboard.ts
+++
@@ -1,2 +0,0 @@
-import Application from "@enhavo/dashboard/Dashboard/DashboardApplication";
-Application.getVueLoader().load(() => import("@enhavo/dashboard/Dashboard/Components/ApplicationComponent.vue"));
